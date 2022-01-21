9bc29789e231e39d105d8c40f46eacda736e61c6
Ludof(desktop-linux) <kaassjoerd@protonmail.com>
2022-01-21 16:57:07 +0100

    Added option to display data. Currently fixed on first year in dataset

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from asyncore import read
+from cProfile import label
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -7,11 +9,13 @@ def df5(x, h, i):
     else:
         return (x[i] - 8*x[i] + 8*x[i] - x[i])/(12*h)
 
+'''
 def ddf5(x, h, i):
     if (i - 2*h) >= 0 and (i + 2*h) < len(x):
         return (-x[i -2*h] + 16*x[i - h] - 30*x[i] + 16*x[i + h] - x[i + 2*h])/(12*pow(h, 2))
     else:
         return (-x[i] + 16*x[i] - 30*x[i] + 16*x[i] - x[i])/(12*pow(h, 2))
+'''
 
 def vrts_gem(q, n, x):
     som = 0
@@ -31,27 +35,55 @@ def vgem(q, n, x):
             continue
     return som/q
 
-dag = []
-temp = []
-fp = open("result.txt", "r")
-for line in fp:
-    _, temp_dag, temp_x = line.split(',')
-    temp_x = int(temp_x)
-    dag_x = str(temp_dag[0:4]) + "-" + str(temp_dag[4:6]) + "-" + str(temp_dag[6:8])
-    temp_x /= 10
-    dag.append(dag_x)
-    temp.append(temp_x)
-h = 1
-afgeleide = []
-for i in range(len(temp[:365])):
-    afgeleide.append(df5(temp, h, i))
-smooth = []
-for r in range(len(temp[:365])):
-    smooth.append(vgem(25, r, temp[:365]))
-smooth_af = []
-for j in range(len(afgeleide)):
-    smooth_af.append(vgem(25, j, afgeleide))
+def read_data(filename):
+    dag = []
+    temp = []
+    fp = open(filename, "r")
+    for line in fp:
+        _, temp_dag, temp_x = line.split(',')
+        temp_x = int(temp_x)
+        dag_x = str(temp_dag[0:4]) + "-" + str(temp_dag[4:6]) + "-" + str(temp_dag[6:8])
+        temp_x /= 10
+        dag.append(dag_x)
+        temp.append(temp_x)
+    return temp
 
+def df_year(year, temp, h):
+    df_data = []
+    for i in range(len(temp[:365])):
+        df_data.append(df5(temp, h, i))
+    return df_data
+    
+def avg_temp(year, temp):
+    avg_temp_data = []
+    for r in range(len(temp[:365])):
+        avg_temp_data.append(vgem(25, r, temp[:365]))
+    return avg_temp_data
+
+def avg_incr_temp(year, df_data):
+    avg_temp_incr_data = []
+    for j in range(len(df_data)):
+        avg_temp_incr_data.append(vgem(25, j, df_data))
+    return avg_temp_incr_data
+
+def plot_data(year, temp, avg_temp_data, df_data, avg_incr_temp_data):
+    figure, axis = plt.subplots(2, 1)
+    axis[0].plot(temp[:365], label=f'Tempature')
+    axis[0].plot(avg_temp_data, label=f'Average temparature')
+    axis[1].plot(df_data, label=f'Increase in temparature')
+    axis[1].plot(avg_incr_temp_data, label=f'Average increase in temparature')
+    axis[0].set_title(f'Tempature stats in {year}')
+    axis[1].set_title(f'Tempature increase stats in {year}')
+    axis[0].legend()
+    axis[1].legend()
+    plt.show()
+
+temp = read_data("result.txt")
+df_data = df_year(0, temp, 1)
+avg_temp_data = avg_temp(0, temp)
+avg_incr_temp_data = avg_incr_temp(0, df_data)
+plot_data(0, temp, avg_temp_data, df_data, avg_incr_temp_data)
+'''
 k = 0
 avg_acc_2_jan_list = []
 avg_temp_1_jan_list = []
@@ -67,10 +99,4 @@ avg_acc_2_jan = sum(avg_acc_2_jan_list)/len(avg_acc_2_jan_list)
 print(avg_temp_1_jan)
 print(avg_acc_2_jan)
 print((1 + avg_acc_2_jan)*avg_temp_1_jan)
-
-figure, axis = plt.subplots(3, 1)
-axis[0].plot(temp[:365])
-axis[0].plot(smooth)
-axis[1].plot(afgeleide)
-axis[1].plot(smooth_af)
-plt.show()
+'''
